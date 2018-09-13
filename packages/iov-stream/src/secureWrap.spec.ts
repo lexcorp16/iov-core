ff50771ded3f7bbf40e7384b41e289b4271f8339
Ethan Frey <ethanfrey@users.noreply.github.com>
2018-09-13 22:35:23 +0200

    Demostrate shortcoming of reflecting variables

--- packages/iov-stream/src/secureWrap.spec.ts
+++ packages/iov-stream/src/secureWrap.spec.ts
@@ -5,22 +5,31 @@ describe("Verify we don't leak secrets", () => {
     expect(foo.isSecret(shared)).toBe(false);
     expect(foo.isSecret(secret)).toBe(true);
     expect(foo.shared).toEqual(shared);
+    // force a write and see we got it
+    // tslint:disable-next-line:no-object-mutation
+    (foo as any).shared = "new";
+    expect(foo.shared).toEqual("new");
   };
 
   it("Can access fields of original variable", () => {
     const foo = new Foo("Public", "Private");
-
     validateFunctionality(foo, "Public", "Private");
+
     const stolen: any = (foo as any).secret;
     expect(stolen).toEqual("Private");
+    // this was reset in validate functionality
+    expect(foo.shared).toEqual("new");
   });
 
   it("Cannot access secret of wrapped variable", () => {
     const foo = new Foo("Public", "Private");
     const secure = secureFoo(foo);
-
     validateFunctionality(secure, "Public", "Private");
+
     const stolen: any = (secure as any).secret;
     expect(stolen).toEqual(undefined);
+    // this was reset in validate functionality
+    expect(secure.shared).toEqual("new");
+    expect(foo.shared).toEqual("new");
   });
 });
